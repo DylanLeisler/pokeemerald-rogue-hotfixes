e880aadb7ff99dc439c325cfbe21c5ea481ecd30
Dylan Leisler <dleisler01@gmail.com>
2024-10-25 20:03:47 -0500

    readding change

--- src/shop.c
+++ src/shop.c
@@ -141,6 +141,7 @@ static void Task_HandleShopMenuQuit(u8 taskId);
 static void CB2_InitBuyMenu(void);
 static void Task_GoToBuyOrSellMenu(u8 taskId);
 static void MapPostLoadHook_ReturnToShopMenu(void);
+static void MapPostLoadHook_ReturnToShopMenuNoFade(void);
 static void Task_ReturnToShopMenu(u8 taskId);
 static void ShowShopMenuAfterExitingBuyOrSellMenu(u8 taskId);
 static void BuyMenuDrawGraphics(void);
@@ -541,7 +542,7 @@ void CB2_ExitSellMenu(void)
 
 void CB2_ExitAutoSellMenu(void)
 {
-    gFieldCallback = MapPostLoadHook_ReturnToShopMenu;
+    gFieldCallback = MapPostLoadHook_ReturnToShopMenuNoFade;
     SetMainCallback2(CB2_ReturnToField);
 }
 
@@ -587,7 +588,7 @@ static void MapPostLoadHook_ReturnToShopMenu(void)
     CreateTask(Task_ReturnToShopMenu, 8);
 }
 
-static void MapPostLoadHook_ReturnToShopMenu(void)
+static void MapPostLoadHook_ReturnToShopMenuNoFade(void)
 {
     CreateTask(Task_ReturnToShopMenu, 8);
 }
